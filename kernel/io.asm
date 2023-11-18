f134b10818934f171c8163a059871870015eb895
jay <jay@example.com>
2023-11-18 06:14:21 +0000

    add in into io

--- kernel/io.asm
+++ kernel/io.asm
@@ -1,12 +1,20 @@
+global inb
+; read bits to I/O port
+;   
+;   [esp    ] : return addr
+;   [esp + 4] : port number to read to
+inb:
+    mov dx, [esp+4]
+    in al, dx
+    ret
+
+
 global outb
-
-
-
 ; write bits to I/O port
 ;   
-;   [esp + 8] : 8 bit value to write
-;   [esp + 4] : port number to write to
 ;   [esp    ] : return addr
+;   [esp + 4] : port number to write to
+;   [esp + 8] : 8 bit value to write
 outb:
     mov dx, [esp+4]
     mov al, [esp+8]
--- kernel/io.h
+++ kernel/io.h
@@ -1,2 +1,3 @@
 #pragma once
 void outb(unsigned short port, char val);
+unsigned char inb(unsigned short port);
